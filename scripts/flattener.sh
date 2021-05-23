82755e5378f714f2c464f34282af69e63adb0906
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2021-05-24 03:22:18 +0700

    Add files via upload

---
+++ scripts/flattener.sh
@@ -0,0 +1,4 @@
+mkdir contracts/.flattened
+npx truffle-flattener contracts/DEXswapRouter.sol > contracts/.flattened/DEXswapRouter.sol
+npx truffle-flattener contracts/libraries/DEXswapLibrary.sol > contracts/.flattened/DEXswapLibrary.sol
+npx truffle-flattener contracts/libraries/DEXswapOracleLibrary.sol > contracts/.flattened/DEXswapOracleLibrary.sol
